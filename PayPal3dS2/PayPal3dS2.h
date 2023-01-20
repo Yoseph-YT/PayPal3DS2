b02c668df1525a80b3cfbeadd8b1d01143f87e9a
Yoseph-YT <73658848+Yoseph-YT@users.noreply.github.com>
2023-01-20 09:59:35 -0800

    Initial Commit

---
+++ PayPal3dS2/PayPal3dS2.h
@@ -0,0 +1,18 @@
+//
+//  PayPal3dS2.h
+//  PayPal3dS2
+//
+//  Created by Yoseph Tilahun on 1/20/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PayPal3dS2.
+FOUNDATION_EXPORT double PayPal3dS2VersionNumber;
+
+//! Project version string for PayPal3dS2.
+FOUNDATION_EXPORT const unsigned char PayPal3dS2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayPal3dS2/PublicHeader.h>
+
+
