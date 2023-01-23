376b380eb7a4e86a5eacde7933083edf5c118cbe
Yoseph-YT <73658848+Yoseph-YT@users.noreply.github.com>
2023-01-23 12:35:49 -0800

    Intial commit.

---
+++ Sources/PayPal3DS2/data/CardInfo.swift
@@ -0,0 +1,63 @@
+//
+//  File.swift
+//  
+//
+//  Created by YOSEPH TILAHUN on 1/22/23.
+//
+
+import Foundation
+
+public enum ResponseStatus {
+    case cardNotSetup
+    case threeDS2Verified
+    case threeDS2NotRequired
+    case threeDS2BankVerification
+    case threeDS2failed
+    case threeDS2UnKnown
+}
+
+extension ResponseStatus {
+   public var statusMessage: String {
+        switch self {
+        case .cardNotSetup:
+            return "3DS2 is not configured for this card."
+        case .threeDS2Verified:
+            return "3DS2 is verified  by the issuer for this card."
+        case .threeDS2NotRequired:
+            return "3DS2 verification is not required."
+        case .threeDS2BankVerification:
+            return "3DS2 Issuing back app verification required."
+        case .threeDS2failed:
+            return "3DS2 verification failed."
+        case .threeDS2UnKnown:
+            return "3DS2 status Unknown."
+        }
+    }
+}
+
+public struct CardInfo {
+    public var id: String
+    public var name: String
+    public var type: String
+    public var cardNumber: String
+    public var threeDS2: String
+    public var cvv: String
+    public var postalCode: String
+    public var expirationDate: String
+    public var responseStatus: ResponseStatus
+    
+    public init (id: String, name: String, type: String, cardNumber: String, threeDS2: String, cvv: String, postalCode: String, expirationDate: String, responseStatus: ResponseStatus = ResponseStatus.threeDS2UnKnown) {
+        self.id = id
+        self.name = name
+        self.type = type
+        self.cardNumber = cardNumber
+        self.threeDS2 = threeDS2
+        self.cvv = cvv
+        self.postalCode = postalCode
+        self.expirationDate = expirationDate
+        self.responseStatus = responseStatus
+    }
+}
+
+
+    
---
+++ Sources/PayPal3DS2/view/ActivityIndicator.swift
@@ -0,0 +1,28 @@
+//
+//  ActivityIndicator.swift
+//  PayPal3dS2
+//
+//  Created by Yoseph Tilahun on 1/20/23.
+//
+
+import SwiftUI
+
+public struct ActivityIndicator: UIViewRepresentable {
+    @Binding var shouldAnimate: Bool
+    var color: UIColor
+    
+    //ActivityIndicator(isAnimating: $isShowing, style: .large)
+  public  func makeUIView(context: Context) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: .large)
+    }
+
+   public func updateUIView(_ uiView: UIActivityIndicatorView,
+                      context: Context) {
+        if self.shouldAnimate {
+            uiView.color = color
+            uiView.startAnimating()
+        } else {
+            uiView.stopAnimating()
+        }
+    }
+}
